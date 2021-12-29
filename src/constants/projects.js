b388622509ab7e350c467ceb2fc92b517ab594c5
Cees Jol <cjol99@icloud.com>
2021-12-29 07:10:34 +0100

    Add SO visualization project

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -17,7 +17,6 @@ const MY_PROJECTS = [
     technologies: "projects.projects.0.technologies",
     link: "https://resume-ivory.vercel.app/",
     sourceLink: "https://github.com/CeesJol/resume",
-    img: "resume.png",
   },
   {
     name: "projects.projects.1.name",
@@ -33,7 +32,6 @@ const MY_PROJECTS = [
     ],
     technologies: "projects.projects.1.technologies",
     link: "https://ikwilroeien.nl",
-    img: "proteus.png",
   },
   {
     name: "projects.projects.2.name",
@@ -46,7 +44,18 @@ const MY_PROJECTS = [
     features: "projects.projects.2.features",
     technologies: "projects.projects.2.technologies",
     sourceLink: "https://github.com/CeesJol/lireddit",
-    img: "lireddit.png",
+  },
+  {
+    name: "projects.projects.3.name",
+    description: [
+      {
+        title: "projects.idea",
+        description: "projects.projects.3.description.0",
+      },
+    ],
+    features: "projects.projects.3.features",
+    link: "https://so2020.ceesjol.nl/",
+    sourceLink: "https://github.com/CeesJol/data-visualisation",
   },
 ];
 
