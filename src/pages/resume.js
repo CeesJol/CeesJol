78bf73ec987eb176c89084bb225b11cc9c9f41d0
Cees Jol <cjol99@icloud.com>
2022-07-05 12:50:37 +0200

    Update resume link

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 
 const Resume = () => {
-  const { t } = useTranslation();
-  if (typeof window !== "undefined") {
-    window.location = t("contact.resume.link");
-  }
+  // const { t } = useTranslation();
+  // if (typeof window !== "undefined") {
+  //   window.location = t("contact.resume.link");
+  // }
   return (
     <div className="container">
       <div className="content-container">
-        <h1>Downloading resume...</h1>
+        {/* <h1>Downloading resume...</h1>
         <p>
           If it takes longer than five seconds, click{" "}
           <a href={t("contact.resume.link")}>here</a>
+        </p> */}
+        <h1>Resume</h1>
+        <p>
+          Please <a href="mailto:info@ceesjol.nl">email me</a> for my resume.
         </p>
       </div>
     </div>
